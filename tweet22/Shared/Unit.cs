fa0ed1a4d14fc863e2bea7516437381a64e83733
danijeljw-rpc <danijel@repasscloud.com>
2023-04-23 21:29:38 +1000

    amend fix for #9

--- tweet22/Shared/Unit.cs
+++ tweet22/Shared/Unit.cs
@@ -4,7 +4,7 @@ namespace tweet22.Shared
 	public class Unit
 	{
 		public int Id { get; set; }
-		public string? Title { get; set; }
+		public string Title { get; set; } = null!;
 		public int Attack { get; set; }
 		public int Defense { get; set; }
 		public int HitPoints { get; set; } = 100;
