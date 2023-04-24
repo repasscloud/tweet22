f6364207e0211ad6e19551b4e8a4b88ea3a48dc3
danijeljw-rpc <danijel@repasscloud.com>
2023-04-24 15:53:22 +1000

    preload units from controller #31 #32

--- tweet22/Client/Services/IUnitService.cs
+++ tweet22/Client/Services/IUnitService.cs
@@ -5,9 +5,10 @@ namespace tweet22.Client.Services
 {
 	public interface IUnitService
 	{
-		IList<Unit> Units { get; }
+		IList<Unit> Units { get; set; }
 		IList<UserUnit> MyUnits { get; set; }
 		void AddUnit(int unitId);
+		Task LoadUnitsAsync();
 	}
 }
 
