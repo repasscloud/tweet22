1abedbe41c30a8e1de79478f37848192bd747c54
danijeljw-rpc <danijel@repasscloud.com>
2023-04-24 22:38:49 +1000

    implement DataContext, #31

---
+++ tweet22/Server/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using tweet22.Shared;
+
+namespace tweet22.Server.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<Unit> Units { get; set; }
+	}
+}
+
