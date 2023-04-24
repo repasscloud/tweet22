9ba4a8306556959885a28c10649f477ebf52ef89
danijeljw-rpc <danijel@repasscloud.com>
2023-04-24 23:43:24 +1000

    add all required controller behaviours

--- tweet22/Server/Controllers/UnitController.cs
+++ tweet22/Server/Controllers/UnitController.cs
@@ -22,11 +22,56 @@ namespace tweet22.Server.Controllers
             _context = context;
         }
 
-        // GET: /<controller>/
+        // GET: /api/<controller>/
+        [HttpGet]
         public async Task<IActionResult> GetUnits()
         {
             var units = await _context.Units.ToListAsync();
             return Ok(units);
+        }
+
+        // POST: /api/<controller>
+        [HttpPost]
+        public async Task<IActionResult> AddUnit(Unit unit)
+        {
+            _context.Units.Add(unit);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.Units.ToListAsync());
+        }
+
+        // PUT: /api/<controller>/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUnit(int id, Unit unit)
+        {
+            var dbUnit = await _context.Units.FirstOrDefaultAsync(u => u.Id == id);
+
+            if (dbUnit == null)
+                return NotFound("Unit with the given Id doesn't exist");
+
+            dbUnit.Title = unit.Title;
+            dbUnit.Attack = unit.Attack;
+            dbUnit.Defense = unit.Defense;
+            dbUnit.BananaCost = unit.BananaCost;
+            dbUnit.HitPoints = unit.HitPoints;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(dbUnit);
+        }
+
+        // DELETE: /api/<controller>/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUnit(int id)
+        {
+            var dbUnit = await _context.Units.FirstOrDefaultAsync(u => u.Id == id);
+
+            if (dbUnit == null)
+                return NotFound("Unit with the given Id doesn't exist");
+
+            _context.Units.Remove(dbUnit);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Units.ToListAsync());
         }
     }
 }
