57088da99c5d548159c3a7cd17b0937ce128bac7
danijeljw-rpc <danijel@repasscloud.com>
2023-04-25 20:25:00 +1000

    build UserUnits on the server with UserUnitController, #52

---
+++ tweet22/Server/Controllers/UserUnit.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Server.Services;
+using tweet22.Server.Data;
+using tweet22.Shared;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace tweet22.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UserUnitController : ControllerBase
+    {
+        private readonly DataContext _context;
+        private readonly IUtilityService _utilityService;
+
+        public UserUnitController(DataContext context, IUtilityService utilityService)
+        {
+            _context = context;
+            _utilityService = utilityService;
+        }
+
+        
+        // POST api/values
+        [HttpPost]
+        public async Task<IActionResult> BuildUserUnit([FromBody]int unitId)
+        {
+            var unit = await _context.Units.FirstOrDefaultAsync<Unit>(u => u.Id == unitId);
+            var user = await _utilityService.GetUser();
+            if (user.Bananas < unit.BananaCost)
+            {
+                return BadRequest("Not enough bananas");
+            }
+
+            user.Bananas -= unit.BananaCost;
+
+            var newUserUnit = new UserUnit
+            {
+                UnitId = unit.Id,
+                UserId = user.Id,
+                HitPoints = unit.HitPoints
+            };
+
+            _context.UserUnits.Add(newUserUnit);
+            await _context.SaveChangesAsync();
+
+            return Ok(newUserUnit);
+        }
+    }
+}
+
