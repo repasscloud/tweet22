42624a93d569dd14a6be92d7b2fb402571fd15a8
danijeljw-rpc <danijel@repasscloud.com>
2023-04-25 17:04:26 +1000

    fixed with git repo, #31

--- tweet22/Client/CustomAuthProvider.cs
+++ tweet22/Client/CustomAuthProvider.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Security.Claims;
 using System.Text.Json;
-using Blazored.LocalStorage;
-using Microsoft.AspNetCore.Components.Authorization;
+using System.Threading.Tasks;
 
 namespace tweet22.Client
 {
@@ -41,18 +45,17 @@ namespace tweet22.Client
 
         private byte[] ParseBase64WithoutPadding(string base64)
         {
-            switch(base64.Length % 4)
+            switch (base64.Length % 4)
             {
                 case 2: base64 += "=="; break;
                 case 3: base64 += "="; break;
             }
-
             return Convert.FromBase64String(base64);
         }
 
         private IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
         {
-            var payload = jwt.Split('.')[0];
+            var payload = jwt.Split('.')[1];
             var jsonBytes = ParseBase64WithoutPadding(payload);
             var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
             var claims = keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString()));
@@ -61,4 +64,3 @@ namespace tweet22.Client
         }
     }
 }
-
--- tweet22/Server/Controllers/AuthController.cs
+++ tweet22/Server/Controllers/AuthController.cs
@@ -1,12 +1,11 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using tweet22.Server.Data;
 using tweet22.Shared;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace tweet22.Server.Controllers
 {
@@ -21,13 +20,11 @@ namespace tweet22.Server.Controllers
             _authRepository = authRepository;
         }
 
-
-        // POST: /api/<controller>/register
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserRegister request)
         {
             var response = await _authRepository.Register(
-                new Shared.User
+                new User
                 {
                     Username = request.Username,
                     Email = request.Email,
@@ -37,17 +34,18 @@ namespace tweet22.Server.Controllers
                 }, request.Password);
 
             if (!response.Success)
+            {
                 return BadRequest(response);
+            }
 
             return Ok(response);
         }
 
-        // POST: /api/<controller>/login
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserLogin request)
         {
             var response = await _authRepository.Login(
-                email: request.Email, request.Password);
+                request.Email, request.Password);
 
             if (!response.Success)
             {
@@ -58,4 +56,3 @@ namespace tweet22.Server.Controllers
         }
     }
 }
-
